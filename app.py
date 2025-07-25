9b7e5656de537a8f7a958baac3d290d29e19b7a9
Mohammed Elbediwi <m.elbediwi@zju.edu.cn>
2025-07-25 15:05:20 +0200

    My first website

--- app.py
+++ app.py
@@ -6,15 +6,9 @@ import pydeck as pkl
 # TaxiFareModel front
 '''
 API_URL = "https://taxifare.lewagon.ai/predict"
-st.markdown('''
-Remember that there are several ways to output content into your web page...
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
 
-'''
-
-'''
+            
 
 
 st.title("🚕 NYC Taxi Fare Predictor")
